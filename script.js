1b39fc882886b609a3b7c7f0ec0d980c16c42e20
Harshcombo50548 <108095091+Harshcombo50548@users.noreply.github.com>
2023-11-26 17:09:09 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,38 +1,16 @@
-// Function to move the background down
+// script.js
 function moveBackgroundDown() {
-    const dayBackground = document.getElementById('dayBackground');
-    const nightBackground = document.getElementById('nightBackground');
-
-    dayBackground.style.animation = 'moveDown 1s forwards';
-    nightBackground.style.animation = 'moveDown 1s forwards';
+    const background = document.getElementById('background');
+    background.style.animation = 'moveDown 1s forwards';
 }
 
-// Function to set the day/night background based on server status
-function setDayNightBackground(isOnline) {
-    console.log('Setting background. Server online status:', isOnline);
-
-    const dayBackground = document.getElementById('dayBackground');
-    const nightBackground = document.getElementById('nightBackground');
-
-    if (isOnline) {
-        console.log('Server is online. Showing day background.');
-        nightBackground.style.display = 'none';
-        dayBackground.style.display = 'block';
-    } else {
-        console.log('Server is offline. Showing night background.');
-        dayBackground.style.display = 'none';
-        nightBackground.style.display = 'block';
-    }
-}
-
-// Function to check server status
 function checkServerStatus() {
     const ipAddress = 'Survivetime.org'; // Domain
     const apiUrl = `https://api.mcsrvstat.us/2/${ipAddress}`;
     const statusElement = document.getElementById('status');
-    const loader = document.getElementById('loader');
 
     // Display loader while checking server status
+    const loader = document.getElementById('loader');
     loader.style.display = 'inline-block';
 
     fetch(apiUrl)
@@ -44,35 +22,18 @@ function checkServerStatus() {
             }
         })
         .then(data => {
-            const isOnline = data.online !== undefined ? data.online : false; // Ensure the correct field is used
-
-            console.log('Server is online:', isOnline);
-
-            // Set day/night background based on server status
-            setDayNightBackground(isOnline);
-
-            // Move background down if the server is online
-            if (isOnline) {
-                moveBackgroundDown();
-            }
-
-            // Hide loader after checking server status
-            loader.style.display = 'none';
+            statusElement.textContent = data.online ? 'Server is online' : 'Server is offline';
         })
         .catch(error => {
             console.error(error);
             statusElement.textContent = 'Error checking server status';
-
-            // Hide loader in case of an error
+        })
+        .finally(() => {
+            // Hide loader after checking server status
             loader.style.display = 'none';
         });
 }
 
-// Initial background check on page load
+// Check server status on page every 5 sec
 checkServerStatus();
-
-// Subsequent background checks every 5 seconds
-setInterval(() => {
-    console.log('Checking server status...');
-    checkServerStatus();
-}, 5000);
+setInterval(checkServerStatus, 5000);
