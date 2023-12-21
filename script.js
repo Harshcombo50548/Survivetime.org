f45f80240736f3f5e810e81764f718cdaa4464da
Harshcombo50548 <108095091+Harshcombo50548@users.noreply.github.com>
2023-12-21 14:34:12 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,46 +1,128 @@
-// Function to make API request and update schedule
-async function updateLoadSheddingSchedule() {
-    const apiUrl = 'https://developer.sepush.co.za/business/2.0/area?id=tshwane-14-wierdapark';
-    const apiToken = '00A61794-FB014AB3-A1B246E4-45428574'; // Replace with your actual API token
+// script.js
+let previousStatus = null; // Variable to store the previous server status
 
+function moveBackgroundDown() {
+    const background = document.getElementById('grass');
+    background.style.animation = 'moveDown 1s forwards';
+}
+
+function showDayOnline() {
+    const dayOnline = document.getElementById('Day/Online');
+    dayOnline.style.display = 'block';
+}
+
+function showNightOffline() {
+    const nightOffline = document.getElementById('night/offline');
+    nightOffline.style.display = 'block';
+}
+
+function hideDayOnline() {
+    const dayOnline = document.getElementById('Day/Online');
+    dayOnline.style.display = 'none';
+}
+
+function hideNightOffline() {
+    const nightOffline = document.getElementById('night/offline');
+    nightOffline.style.display = 'none';
+}
+
+function checkServerStatus() {
+    const ipAddress = 'Survivetime.org'; // Domain
+    const apiUrl = `https://api.mcsrvstat.us/2/${ipAddress}`;
+    const statusElement = document.getElementById('status');
+
+    // Display loader while checking server status
+    const loader = document.getElementById('loader');
+    loader.style.display = 'inline-block';
+
+// check server online ofline status, uncomment it to make it actually work
+
+
+    fetch(apiUrl)
+        .then(response => {
+           if (response.ok) {
+               return response.json();
+            } else {
+              throw new Error('Server not reachable');
+          }
+       })
+        .then(data => {
+            if (data.online) {
+               statusElement.textContent = 'Server is online';
+                if (previousStatus !== true) {
+                    showDayOnline();
+                    hideNightOffline();
+                }
+                previousStatus = true;
+            } else {
+                statusElement.textContent = 'Server is offline';
+               if (previousStatus !== false) {
+                    showNightOffline();
+                    hideDayOnline();
+                }
+                previousStatus = false;
+            }
+       })
+        .catch(error => {
+            console.error(error);
+            statusElement.textContent = 'Error checking server status';
+        })
+        .finally(() => {
+            // Hide loader after checking server status
+            loader.style.display = 'none';
+       });
+
+
+  
+
+
+}
+
+// Check server status on page every 5 sec
+checkServerStatus();
+setInterval(checkServerStatus, 5000);
+
+
+
+
+ const eskomSePushWorkerUrl = 'https://worker-bitter-mode-351b.harshcombo50548.workers.dev/'; // Replace with your Cloudflare Worker URL
+
+// Function to make API request to Cloudflare Worker
+async function getLoadSheddingInfo() {
     try {
-        // Make API request
-        const response = await fetch(apiUrl, {
-            headers: {
-                'Token': apiToken,
-            },
-        });
-
-        if (response.ok) {
-            // Parse JSON response
-            const data = await response.json();
-            const schedule = generateScheduleHTML(data);
-            
-            // Update HTML with schedule
-            document.getElementById('loadSheddingSchedule').innerHTML = schedule;
-        } else {
-            console.error('Error fetching data from API:', response.statusText);
+        const response = await fetch(eskomSePushWorkerUrl);
+        
+        if (!response.ok) {
+            throw new Error(`HTTP error! Status: ${response.status}`);
         }
+
+        const data = await response.json();
+        return data;
     } catch (error) {
-        console.error('Error:', error.message);
+        console.error('Error fetching load shedding information:', error.message);
+        return null;
     }
 }
 
-// Function to generate HTML for load shedding schedule
-function generateScheduleHTML(data) {
-    // Modify this function based on the API response structure
-    // Use data to extract load shedding schedule information
-    // Return HTML string representing the schedule
+// Function to update the HTML with load shedding information
+function updateLoadSheddingInfo(info) {
+    const loadSheddingInfoElement = document.getElementById('loadSheddingInfo');
 
-    // Example: Displaying the first event's start and end times
-    const event = data.events[0];
-    const scheduleHTML = `<p>Load shedding today from ${event.start} to ${event.end}</p>`;
-    
-    return scheduleHTML;
+    if (info && info.events && info.events.length > 0) {
+        const event = info.events[0];
+        const html = `<p>Load Shedding Event: ${event.note}, Start: ${event.start}, End: ${event.end}</p>`;
+        loadSheddingInfoElement.innerHTML = html;
+    } else {
+        loadSheddingInfoElement.innerHTML = '<p>No load shedding information available.</p>';
+    }
 }
 
-// Initial update when the page loads
-updateLoadSheddingSchedule();
+// Initial load shedding info update
+getLoadSheddingInfo()
+    .then(updateLoadSheddingInfo);
 
-// Schedule periodic updates (every hour in this example)
-setInterval(updateLoadSheddingSchedule, 3600000); // 3600000 milliseconds = 1 hour
+// Update load shedding info every hour
+setInterval(async () => {
+    const updatedInfo = await getLoadSheddingInfo();
+    updateLoadSheddingInfo(updatedInfo);
+}, 3600000); // 3600000 milliseconds = 1 hour */
